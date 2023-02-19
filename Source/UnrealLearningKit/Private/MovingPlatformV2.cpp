66d20d89c9ac3f093dec29700f4505be08c1e2f2
mtafsir <49006769+mtafsir@users.noreply.github.com>
2023-02-19 17:29:19 -0600

    Build Landscape and Clean Up Code
    Build Landscape and Clean Up Code

--- Source/UnrealLearningKit/Private/MovingPlatformV2.cpp
+++ Source/UnrealLearningKit/Private/MovingPlatformV2.cpp
@@ -6,8 +6,12 @@
 // Sets default values
 AMovingPlatformV2::AMovingPlatformV2()
 {
+	FString Name = GetName();
+
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	UE_LOG(LogTemp, Display, TEXT("BP %s has been initialized"), *Name);
 
 }
 
@@ -31,13 +35,22 @@ void AMovingPlatformV2::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	
+	AMovingPlatformV2::MovePlatform(DeltaTime);
 
+	AMovingPlatformV2::RotatePlatform(DeltaTime);
+
+
+
+
+}
+
+void AMovingPlatformV2::MovePlatform(float DeltaTime)
+{
 	// Get current location
 	FVector CurrentLocation = GetActorLocation();
-		
-	
-	if(Direction == AMovingPlatformV2::MovingDirection::Forward)
+
+
+	if (Direction == AMovingPlatformV2::MovingDirection::Forward)
 	{
 		CurrentLocation = CurrentLocation + (PlatformVelocity * DeltaTime);
 		FVector test = (PlatformVelocity * DeltaTime);
@@ -57,18 +70,18 @@ void AMovingPlatformV2::Tick(float DeltaTime)
 
 	// Sets direction based on distance from the start
 	//FVector::Dist(PlatformVelocity,FVector(0)) -> This will return the distance the platform will move
-	if( DistanceFromTheStartCurrent >= Distance)
-	{	
+	if (DistanceFromTheStartCurrent >= Distance)
+	{
 		CurrentLocation = StartLocation + MoveDirectionNormal * DistanceFromTheStartCurrent;
 		SetActorLocation(CurrentLocation);
 
 		Direction = AMovingPlatformV2::MovingDirection::Backwards;
-		
+
 		// A flag that a positive distance in comparison to the previous run should not appear again without the direction being changed
 		PositiveCheck = 1;
 	}
 	else if (DistanceFromTheStartCurrent >= 0 && (0 < DistanceFromTheStartCurrent - DistancePrevious) && PositiveCheck == 1)
-	{	
+	{
 		SetActorLocation(StartLocation);
 
 		Direction = AMovingPlatformV2::MovingDirection::Forward;
@@ -77,6 +90,9 @@ void AMovingPlatformV2::Tick(float DeltaTime)
 
 	DistancePrevious = DistanceFromTheStartCurrent;
 
-
 }
 
+void AMovingPlatformV2::RotatePlatform(float DeltaTime)
+{
+	UE_LOG(LogTemp, Display, TEXT("%s Rotating....", *GetName());
+}
